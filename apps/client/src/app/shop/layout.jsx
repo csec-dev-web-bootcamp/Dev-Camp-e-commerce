14f7d95b099ad2abc05160084c26c8f502d57e25
BereketW <psycheciphers@gmail.com>
2024-05-10 07:18:19 -0700

    Dynamic routing on detail and add to cart

--- apps/client/src/app/shop/layout.jsx
+++ apps/client/src/app/shop/layout.jsx
@@ -1,19 +1,18 @@
 "use client"; // New line at the top
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { MdManageAccounts } from "react-icons/md";
 import { GiFemaleVampire } from "react-icons/gi";
 import { FcElectronics } from "react-icons/fc";
 import { GiBigDiamondRing } from "react-icons/gi";
-import Link from 'next/link';
+import Link from "next/link";
 import { useCollapsed } from "@app/client/store/collapse";
 import { useSelectedMenu } from "@app/client/store/selectedMenuItem";
 
 const about = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); 
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const { isCollapsed, setIsCollapsed } = useCollapsed();
   const { selectedMenuItem, setSelectedMenuItem } = useSelectedMenu();
-  
 
   const handleMenuClick = (menuItem) => {
     setSelectedMenuItem(menuItem.key);
@@ -22,44 +21,51 @@ const about = ({ children }) => {
     setIsSidebarOpen(!isSidebarOpen); // Ensure state update
   };
 
-  
   return (
     <>
-      <div className='flex  shadow-lg'>
-        <div className='col-span-1 bg-gray-300 p-6 px-9 '>
-          <div className='p-4'>side bar </div>
+      <div className="flex  shadow-lg">
+        <div className="col-span-1 bg-gray-300 p-6 px-9 ">
+          <div className="p-4">side bar </div>
           <Link href="/shop/mensClothing">
-            <div className='flex my-3 hover:text-blue-500 border-white-500'> 
-                  <div><MdManageAccounts /></div> 
-                  <div>Male Cloth</div> 
+            <div className="flex my-3 hover:text-blue-500 border-white-500">
+              <div>
+                <MdManageAccounts />
+              </div>
+              <div>Male Cloth</div>
             </div>
           </Link>
           <Link href="/shop/womenClothing">
-              <div className='flex mb-3 hover:text-blue-500 border-white-500'> 
-                  <div><GiFemaleVampire /></div>
-                  <div>Female Cloth</div> 
+            <div className="flex mb-3 hover:text-blue-500 border-white-500">
+              <div>
+                <GiFemaleVampire />
               </div>
+              <div>Female Cloth</div>
+            </div>
           </Link>
           <Link href="/shop/jewelery">
-            <div className='flex mb-3 hover:text-blue-500 border-white-500'> 
-                <div><GiBigDiamondRing /></div>
-                <div>Jewllery</div> 
+            <div className="flex mb-3 hover:text-blue-500 border-white-500">
+              <div>
+                <GiBigDiamondRing />
+              </div>
+              <div>Jewllery</div>
             </div>
           </Link>
           <Link href="/shop/electronics">
-            <div className='flex mb-3 hover:text-blue-500 border-white-500'> 
-                <div><FcElectronics /></div>
-                <div>Electronics</div> 
+            <div className="flex mb-3 hover:text-blue-500 border-white-500">
+              <div>
+                <FcElectronics />
+              </div>
+              <div>Electronics</div>
             </div>
           </Link>
         </div>
-        <div className='shadow-lg w-full'> 
-          <FaBars  className='cursor-pointer' onClick={toggleSidebar} />
-          <p >{ children }</p>
+        <div className="shadow-lg w-full">
+          <FaBars className="cursor-pointer" onClick={toggleSidebar} />
+          <p>{children}</p>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default about
+export default about;
