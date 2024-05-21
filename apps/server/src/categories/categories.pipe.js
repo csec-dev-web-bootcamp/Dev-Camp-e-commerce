d2a6c92e3bec7e97ca1879003261f350c185f0f3
Firomsa Hika <firomsahika2022@gmail.com>
2024-05-21 21:38:12 +0300

    added

--- apps/server/src/categories/categories.pipe.js
+++ apps/server/src/categories/categories.pipe.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import {createCategorySchema , formatZodError} from "@repo/common";
-import { HttpException } from "../common/http-exception";
-
-export function createCategoryPipe(req,res,next){
-    const data = req.body;
-    const result = createCategorySchema.safeParse(data);
-    if(!result.success){
-        throw new HttpException(formatZodError(result.errior), 400);
-    }
-    req.body = result.data;
-    next();
-}
-
-export function updateCategoryPipe(req,res,next){
-    const data = req.body;
-    const result = createCategorySchema.safeParse(data);
-    if(!result.success){
-        throw new HttpException(formatZodError(result.errior), 400);
-    }
-    req.body = result.data;
-    next();
-=======
 import { createCategorySchema, formatZodError } from "@repo/common";
 import { HttpException } from "../common/http-exception";
 
@@ -42,5 +19,4 @@ export function updateCategoryPipe(req, res, next) {
   }
   req.body = result.data;
   next();
->>>>>>> c8bfd8e7c21b4333862670d095532b30b064778e
 }
--- apps/server/src/categories/categories.service.js
+++ apps/server/src/categories/categories.service.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import {Prisma} from '@prisma/client';
-
-import prisma from '../config/prismaclient';
-
-export async function createCategory(data){
-    const category  = await prisma.category.create({
-        data:data,
-    });
-    return category;
-}
-
-export async function getManyCategories(){
-    const categories = await prisma.category.findMany();
-    return categories;
-}
-
-export async function getOneCategory(id){
-    const category = await prisma.category.findFirst({where: {id}});
-    return category;
-}
-
-export async function  updateCategory(id, data){
-    const category = await prisma.category.update({
-        where:{id},
-        data:data,
-    });
-    return category;
-}
-
-export async function deleteCategory(id){
-    const category = await prisma.category.delete({
-        where: { id }
-    });
-    return category;
-}
-=======
 import { Prisma } from '@repo/prisma-client';
 import prisma from '../config/prisma-client';
 
@@ -72,4 +35,3 @@ export async function deleteCategory(id) {
   const category = await prisma.category.delete({ where: { id } });
   return category;
 }
->>>>>>> c8bfd8e7c21b4333862670d095532b30b064778e
